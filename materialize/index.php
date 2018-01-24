2b842eb032d2e51fc7dfc8be373c4d5518cdd630
sudhina <sudhinasathyanm@gmail.com>
2018-01-24 12:26:24 +0530

    intial commit,added files

---
+++ materialize/index.php
@@ -0,0 +1,57 @@
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+  <!-- Compiled and minified JavaScript -->
+ 	 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+          
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <style type="text/css">
+      .circle
+      {
+      	width: 100px;
+      	height: 250px;
+      }
+      	.ilike-blue-container {
+    @extend .blue, .lighten-4;}
+  
+  
+      </style>
+    </head>
+
+    <body>
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <div class="card-panel teal lighten-2 hoverable" align="center">
+      <img src="p.jpeg" alt="" class="circle responsive-img z-depth-5" align="left">
+      This is a card panel with a teal lighten-2 class
+      </div>
+    <div class="container">
+         <img class="card-panel  hoverable responsive-img"  src="Tulips.jpg">
+        <!-- Page Content goes here -->
+     <div class="row">
+      <div class="col s1">1</div>
+      <div class="col s1">2</div>
+      <div class="col s1">3</div>
+      <div class="col s1">4</div>
+      <div class="col s1">5</div>
+      <div class="col s1">6</div>
+      <div class="col s1">7</div>
+      <div class="col s1">8</div>
+      <div class="col s1">9</div>
+      <div class="col s1">10</div>
+      <div class="col s1">11</div>
+      <div class="col s1">12</div>
+    </div>
+   	</div>
+   
+</body>
+  </html>
